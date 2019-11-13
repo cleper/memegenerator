7bd8ecdc74c61376f98eec74e2b0bed5d3c93a02
Lukas Ralys <lukasralys@gmail.com>
2019-11-13 12:59:19 +0200

    Updates

---
+++ precache-manifest.665b9defe56f141cc85537f6b476ca46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6ced0195d52fb9155ec2543cba5d92",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "773f2239b87b580b8234",
+    "url": "/memegenerator/static/js/2.58df7264.chunk.js"
+  },
+  {
+    "revision": "ad05bd1f51dd96bb2a7b",
+    "url": "/memegenerator/static/js/main.f9ebedb1.chunk.js"
+  },
+  {
+    "revision": "a69f4eaab339a2a1787b",
+    "url": "/memegenerator/static/js/runtime-main.4950a7c5.js"
+  },
+  {
+    "revision": "c112622e0ab11666cf8671c48a1d2fec",
+    "url": "/memegenerator/static/media/fjalloneregular.c112622e.ttf"
+  },
+  {
+    "revision": "7f1e5c9a58a8788f5e171c7b43e12ac9",
+    "url": "/memegenerator/static/media/memebackground.7f1e5c9a.svg"
+  }
+]);
