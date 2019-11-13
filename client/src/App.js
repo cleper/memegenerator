cec05e5af23b5f999ce0c82f85998dabf575539c
Lukas Ralys <lukasralys@gmail.com>
2019-11-13 12:52:55 +0200

    Made some simple naming fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Reset from './components/reset/index';
 import LandingView from './components/landingView/index';
-import CssVariables from './components/cssVariables/index';
+import CssVariables from './components/cssvariables/index';
 
 function App() {
+  
   return (
     <React.Fragment>
       <Reset/>
--- client/src/components/cssVariables/index.js
+++ client/src/components/cssVariables/index.js
@@ -1,12 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
-import FjallOneRegular from '../../assets/fonts/FjallaOne-Regular.ttf';
+import fjalloneregular from '../../assets/fonts/fjalloneregular.ttf';
 
 const CssVariables = createGlobalStyle `
 
     @font-face {
         font-family: 'FjallOne-Regular';
         font-style: normal;
-        src: url(${FjallOneRegular}) format('truetype');
+        src: font-url(${fjalloneregular}) format('truetype');
     }
 
     :root{
